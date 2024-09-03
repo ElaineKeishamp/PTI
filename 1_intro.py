2e060a8fa24ebcd9f718133ea8a647c12698b3c9
Elainekeishamp <keishapasaribu09@gmail.com>
2024-09-03 12:34:25 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,18 @@
+# Deklarasi Variabel
+a = float(10.5)
+b = 5
+c = a*b
+# Operasi (Operation) -> Tambah (+), kurang (-), bagi(/), kali (*), modulo (%), pangkat (**)
+#print(c)
+
+Perkalian = a * b
+Pembagian = a / b
+Pengurangan = a - b
+Modulo = a % b
+Pangkat = a ** b
+
+print(Perkalian)
+print(Pembagian)
+print(Pengurangan)
+print(Modulo)
+print(Pangkat)
